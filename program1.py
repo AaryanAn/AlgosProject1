42176200642d8d71d222bebb1877a3da776d11f3
Aaryan Anand <aaryan.r.anand@gmail.com>
2024-10-14 16:37:28 -0400

    current implementation p1

--- program1.py
+++ program1.py
@@ -17,20 +17,37 @@ def program1(n: int, W: int, heights: List[int], widths: List[int]) -> Tuple[int
     """
     ############################
     num_of_platforms = []
-    width_for_platform = 0
-    max_height_for_platform = 0
+    curr_width_for_platform = 0
+    curr_height_for_platform = 0
     curr_paintings_total = 0
     paintings_per_platform = []
 
     for i in range(n):
         # Create new platform if current platform is FULL
-        if widths[i] + width_for_platform > W:
+        if widths[i] + curr_width_for_platform > W:
             paintings_per_platform.append(curr_paintings_total)
 
-            num_of_platforms.append(max_height_for_platform)
+            num_of_platforms.append(curr_height_for_platform)
 
             # Reset parameters for next platform
-            
+
+            curr_width_for_platform = 0
+            curr_height_for_platform = 0
+            curr_paintings_total = 0
+
+        # Append all values to the current shelf (this will be initial since if statement will not be triggered first time)
+
+        curr_paintings_total += 1
+        curr_width_for_platform += widths[i]
+        curr_height_for_platform += max(curr_height_for_platform, heights[i])
+        
+        # We must add the last shelf on its own since we rely on the next shelf being finished 
+        if curr_paintings_total > 0:
+            num_of_platforms.append(curr_height_for_platform)
+            paintings_per_platform.append(curr_paintings_total)
+
+        return len(num_of_platforms), sum(num_of_platforms), paintings_per_platform
+
 
     # Add you code here
     ############################
