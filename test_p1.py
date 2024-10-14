caed86ef7bbca859d96315d6e271f626818abd62
Bishoy P <bishoypramanik@gmail.com>
2024-10-14 16:37:23 -0400

    updating the basic test for p1

--- test_p1.py
+++ test_p1.py
@@ -1,5 +1,6 @@
 import program1
 
-def p1_dummy(self):
-    assert 1+1 == 2
-    
+def test_p1_given():
+    heights = [21, 19, 17, 16, 11, 5, 1]
+    widths = [7, 1, 2, 3, 5, 8, 1]
+    assert program1(n = 7, W = 10, heights = heights, widths = widths) == (3, 42, [3,2,2])
